697d7235bed6d82b917f87b42feff47c8c72780f
Arlaine <44557452+Arlaine@users.noreply.github.com>
2019-02-11 09:26:20 +0800

    Create plot5.R

---
+++ plot5.R
@@ -0,0 +1,31 @@
+> setwd("./R/Coursera/Exploratory Data Analysis/Course Project 2")
+> library("data.table")
+data.table 1.11.8  Latest news: r-datatable.com
+> path <- getwd()
+> download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
++ destfile = paste(path, "PM2.5Files.zip", sep = "/"))
+trying URL 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
+Content type 'application/zip' length 30643310 bytes (29.2 MB)
+downloaded 29.2 MB
+
+> unzip(zipfile = "PM2.5Files.zip")
+> SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+> NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+> library(ggplot2)
+> vehicles <- grepl(pattern = "vehicle",x = SCC$SCC.Level.Two,ignore.case = TRUE)
+> vehiclesSCC <- SCC[vehicles,]$SCC
+> vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
+> baltimorevehiclesnei <- (vehiclesNEI[vehiclesNEI$fips == "24510",])
+> 
+> png(filename = "plot5.png",width = 480, height = 480,units = "px",)
+> 
+> ggplot(data = baltimorevehiclesnei, aes(factor(year), Emissions))+
++ geom_bar(stat = "identity",fill = "grey", width = 0.5)+
++ xlab("Year")+
++ ylab("Total Emissions")+
++ labs(title="Baltimore City Vehicle Source Emission Emissions(1999-2008)")
+> dev.off()
+null device 
+          1 
+> 
